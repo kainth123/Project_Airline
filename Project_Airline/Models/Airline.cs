e9ca003458c3e81bf978a687bc814d48bbce3dae
kainth123 <71673200+kainth123@users.noreply.github.com>
2021-04-19 13:42:52 +1200

    Edit comments in the  model class.

--- Project_Airline/Models/Airline.cs
+++ Project_Airline/Models/Airline.cs
@@ -9,11 +9,17 @@ namespace Project_Airline.Models
     public class Airline
     {
         [Key]
+        //This is the primary key
         public int Id { get; set; }
+        //This is the variable to store the airline name
         public string airline_Name { get; set; }
+        //This is the variable to store the airline email
         public string Email { get; set; }
+        //This is the variable to store the airline mobile
         public int Mobile { get; set; }
+        //This is the variable to store the airline ticket name
         public string ticket_Name { get; set; }
+        //This is the variable to store the airline ticket cost
         public int cost { get; set; }
 
     }
